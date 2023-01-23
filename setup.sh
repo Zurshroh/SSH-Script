abf2acabd7f15a589751343ebb90b1f7e81ebded
Autis <122764123+Locu-Locu@users.noreply.github.com>
2023-01-23 19:53:12 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,6 @@ dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Dat
 
 from keygen import keys
 
-if keys.valid('email', 'license_key', 'public_key'):
-    pass    # The license key is valid.
-else:
-    pass    # The license key is invalid.
 # // Exporting Language to UTF-8
 export LANG='en_US.UTF-8'
 export LANGUAGE='en_US.UTF-8'
@@ -60,7 +56,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$( curl -s https://ipinfo.io/ip/ );
 IZIN=$( curl https://raw.githubusercontent.com/Zuz99/permission/main/ipmini | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
+if [$MYIP = $IZIN]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
 echo -e "${red}Permission Denied!${NC}";
