778c5d5111730fd2223ffd1f7fd337d37289e86b
Autis <122764123+Locu-Locu@users.noreply.github.com>
2023-01-23 19:10:33 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -52,9 +52,8 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$( curl -s https://ipinfo.io/ip/ );
-IZIN=$( curl https://raw.githubusercontent.com/Zuz99/permission/main/ipmini | grep $MYIP )
-[$MYIP = $IZIN]; then
-if
+IZIN=$( curl https://raw.githubusercontent.com/Zuz99/permission/main/ipmini | grep $MYIP );
+if [$MYIP = $IZIN]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
 echo -e "${red}Permission Denied!${NC}";
