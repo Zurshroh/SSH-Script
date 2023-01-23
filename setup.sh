6d61b0d67d65fac1cb69c325b5ee1ab7c5bc1c43
Autis <122764123+Locu-Locu@users.noreply.github.com>
2023-01-23 18:33:33 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
