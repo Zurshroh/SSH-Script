ad04a294969c27238367835cec0acfff519208e5
Autis <122764123+Locu-Locu@users.noreply.github.com>
2023-01-23 17:53:13 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 
-#izin
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
 MYIP=$(wget -qO- ipinfo.io/ip);
 IZIN=$( curl https://raw.githubusercontent.com/Zuz99/permission/main/ipmini | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
