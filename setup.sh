e736369d6297b7aefbf135012d1785dec05565d8
Autis <122764123+Locu-Locu@users.noreply.github.com>
2023-01-23 20:00:57 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -89,7 +89,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl https://raw.githubusercontent.com/zuz99/permision/main/ipmin | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/Locu-Locu/permision/main/ipmin | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 clear
 echo -e "${green}Tahniah! Anda Dibenarkan menggunakan Script Ctech Didik...${NC}"
